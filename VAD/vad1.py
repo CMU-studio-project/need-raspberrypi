31f615909f9a101b9f7c815b0ff0dddc34c1433f
jel-lambda <sheee724@gmail.com>
2023-02-01 12:54:27 -0500

    feat: add auto-record

--- VAD/vad1.py
+++ VAD/vad1.py
@@ -2,6 +2,10 @@ import pvporcupine
 import pyaudio
 import struct
 import numpy as np
+import wave
+#import sounddevice as sd
+#import wavio as wv
+#from scipy.io.wavfile import write
 
 porcupine = pvporcupine.create(
   access_key='R/RPKdlOkQv8mSmUef6ccnRV27swlnk/WG0YDg4z56P1ZVToo7HugA==',
@@ -50,6 +54,12 @@ audio_stream=pa.open(
     input=True,
     frames_per_buffer= new_frame_number)
 
+# Sampling frequency
+freq = 44100
+  
+# Recording duration
+duration = 5
+
 while True:
     pcm = audio_stream.read(new_frame_number)
     pcm = struct.unpack_from("h" * new_frame_number, pcm)
@@ -61,5 +71,52 @@ while True:
     if keyword_index == 0:
         # detected 'hey dobby'
         print('hey dobby detected!')
+        # the file name output you want to record into
+        filename = "recorded.wav"
+        # set the chunk size of 1024 samples
+        chunk = 44100
+        # sample format
+        FORMAT = pyaudio.paInt16
+        # mono, change to 2 if you want stereo
+        channels = 1
+        # 44100 samples per second
+        sample_rate = 44100
+        record_seconds = 5
+        # initialize PyAudio object
+        #p = pyaudio.PyAudio()
+        # open stream object as input & output
+        #stream = pa.open(format=FORMAT,
+        #                channels=channels,
+        #                rate=sample_rate,
+        #                input=True,
+        #                output=True,
+        #                frames_per_buffer=chunk)
+        frames = []
+        print("Recording...")
+        for i in range(int(sample_rate / chunk * record_seconds)):
+            data = audio_stream.read(chunk)
+            # if you want to hear your voice while recording
+            # stream.write(data)
+            frames.append(data)
+        # frames = pcm
+        print("Finished recording.")
+        # stop and close stream
+        # stream.stop_stream()
+        # stream.close()
+        # terminate pyaudio object
+        # pa.terminate()
+        # save audio file
+        # open the file in 'write bytes' mode
+        wf = wave.open(filename, "wb")
+        # set the channels
+        wf.setnchannels(channels)
+        # set the sample format
+        wf.setsampwidth(pa.get_sample_size(FORMAT))
+        # set the sample rate
+        wf.setframerate(sample_rate)
+        # write the frames as bytes
+        wf.writeframes(b"".join(frames))
+        # close the file
+        wf.close()
     else:
         print('nay')
