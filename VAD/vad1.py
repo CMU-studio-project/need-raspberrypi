203acbc788cca69180ebed243afc1122207a0c51
jel-lambda <sheee724@gmail.com>
2023-02-08 20:34:19 -0500

    added korean hot word

--- VAD/vad1.py
+++ VAD/vad1.py
@@ -6,13 +6,42 @@ import wave
 import math
 import pvcobra
 import sounddevice as sd
-from statistics import median
+from picovoice import Picovoice
 
 cobra = pvcobra.create(access_key='R/RPKdlOkQv8mSmUef6ccnRV27swlnk/WG0YDg4z56P1ZVToo7HugA==')
 
 porcupine = pvporcupine.create(
   access_key='R/RPKdlOkQv8mSmUef6ccnRV27swlnk/WG0YDg4z56P1ZVToo7HugA==',
   keyword_paths=['./hey-dobby_en_raspberry-pi_v2_1_0.ppn']
+)
+
+def wake_word_callback():
+    # wake word detected
+    pass
+
+def inference_callback(inference):
+   if inference.is_understood:
+      intent = inference.intent
+      slots = inference.slots
+      # take action based on intent and slot values
+   else:
+      # unsupported command
+      pass
+
+#handle = Picovoice(
+#    access_key='xMMjYslGKCWniL+vZz61i/XTVGTB5vrrsxi11C7q7g/dhBTJxkd84g==',
+#    keyword_path='./ko_raspberry-pi_v2_1_1.ppn', 
+#    wake_word_callback=wake_word_callback, 
+#    context_path="/home/pi8/VAD/dobby_ko_raspberry-pi_v2_1_0.rhn", 
+#    inference_callback=inference_callback,
+#    porcupine_model_path="/home/pi8/VAD/porcupine_params_ko.pv",
+#    rhino_model_path="")
+
+
+porcupine2 = pvporcupine.create(
+  access_key='xMMjYslGKCWniL+vZz61i/XTVGTB5vrrsxi11C7q7g/dhBTJxkd84g==',
+  keyword_paths=['./ko_raspberry-pi_v2_1_1.ppn'],
+  model_path="/home/pi8/VAD/porcupine_params_ko.pv"
 )
 
 
@@ -45,15 +74,13 @@ def resample(data, input_rate=44100):
     
 pa = pyaudio.PyAudio()
 
-new_frame_number = int(porcupine.frame_length * 441 / 160)
+new_frame_number = int(porcupine2.frame_length * 441 / 160)
 
 audio_stream=pa.open(
-    # rate=porcupine.sample_rate,
     rate = 44100,
     channels=1,
     format=pyaudio.paInt16,
     input=True,
-    #stream_callback=callback,
     frames_per_buffer= new_frame_number)
 
 
@@ -66,26 +93,28 @@ while audio_stream.is_active():
     numpydata = np.frombuffer(pcm, dtype=np.int16)
 
     pcm = struct.unpack_from("h" * new_frame_number, pcm)
-
+    
     pcm = resample(pcm, 44100)
 
     volume_queue.insert(0, np.linalg.norm(numpydata))
     voice_prob_queue.insert(0, cobra.process(pcm))
-    if len(volume_queue) > 20:
+    if len(volume_queue) > 40:
         volume_queue.pop()
         voice_prob_queue.pop()
     
     #print(int(np.linalg.norm(numpydata)/10000)*"|")
+    print(cobra.process(pcm))
 
     keyword_index = porcupine.process(pcm)
+    keyword_index2 = porcupine2.process(pcm)
     
     if keyword_index == 0:
         
         # detected 'hey dobby'
         print('hey dobby detected!')
 
-        #print(volume_queue)
-        #print(voice_prob_queue)
+        print(volume_queue)
+        print(voice_prob_queue)
 
         #핫워드가 포착되는 순간은 오히려 "hey dobby"가 끝나는 시점이기 때문에 발음을 시작하던 순간을 기준으로 잡음
         initial_voice_prob  = sorted(voice_prob_queue)[-5]
@@ -109,17 +138,17 @@ while audio_stream.is_active():
         frames = []
         silence = []
 
-        record_seconds = 50
+        record_seconds = 10000
 
-        auto_stop_condition = 5
+        auto_stop_condition = 30
 
         breaker = True
         i = 0
         print("Recording...")
         while breaker and i in range(int(sample_rate / chunk * record_seconds)):
             
-            data = audio_stream.read(chunk , exception_on_overflow = False)
-            
+            data = audio_stream.read(44100, exception_on_overflow = False)
+
             if np.linalg.norm(np.frombuffer(audio_stream.read(new_frame_number, exception_on_overflow = False), dtype=np.int16)) < initial_volume and cobra.process(pcm) < initial_voice_prob:
                 print("||||" * (len(silence) + 1))
                 print("voice prob:",cobra.process(pcm))
@@ -127,7 +156,79 @@ while audio_stream.is_active():
                 silence.append(1)
             else:
                 silence=[]
-                #print(silence)
+                print(silence)
+            
+            #print(silence)
+            if len(silence)==auto_stop_condition:
+                breaker = False
+            # if you want to hear your voice while recording
+            # stream.write(data)
+            frames.append(data)
+            i += 1
+        # frames = pcm
+        print("Finished recording.")
+
+        wf = wave.open(filename, "wb")
+        # set the channels
+        wf.setnchannels(channels)
+        # set the sample format
+        wf.setsampwidth(pa.get_sample_size(FORMAT))
+        # set the sample rate
+        wf.setframerate(sample_rate)
+        # write the frames as bytes
+        wf.writeframes(b"".join(frames))
+        # close the file
+        wf.close()
+    elif keyword_index2 == 0:
+        
+        # detected 'hey dobby'
+        print('도비야 detected!')
+
+        print(volume_queue)
+        print(voice_prob_queue)
+
+        #핫워드가 포착되는 순간은 오히려 "hey dobby"가 끝나는 시점이기 때문에 발음을 시작하던 순간을 기준으로 잡음
+        initial_voice_prob  = sorted(voice_prob_queue)[-5]
+        #initial_volume      = sorted(volume_queue)[-10]
+        initial_volume      = volume_queue[voice_prob_queue.index(sorted(voice_prob_queue)[-5]) + 1]
+
+        print("initial voice prob:", initial_voice_prob)
+        print("initial volume:", initial_volume)
+
+        # the file name output you want to record into
+        filename = "recorded.wav"
+        # set the chunk size of 1024 samples
+        chunk = 44100
+        # sample format
+        FORMAT = pyaudio.paInt16
+        # mono, change to 2 if you want stereo
+        channels = 1
+        # 44100 samples per second
+        sample_rate = 44100
+
+        frames = []
+        silence = []
+
+        record_seconds = 10000
+
+        auto_stop_condition = 30
+
+        breaker = True
+        i = 0
+        print("Recording...")
+        while breaker and i in range(int(sample_rate / chunk * record_seconds)):
+            
+            data = audio_stream.read(44100, exception_on_overflow = False)
+
+            if np.linalg.norm(np.frombuffer(audio_stream.read(new_frame_number, exception_on_overflow = False), dtype=np.int16)) < initial_volume and cobra.process(pcm) < initial_voice_prob:
+                print("||||" * (len(silence) + 1))
+                print("voice prob:",cobra.process(pcm))
+                print("volume:",np.linalg.norm(np.frombuffer(audio_stream.read(new_frame_number, exception_on_overflow = False), dtype=np.int16)))
+                silence.append(1)
+            else:
+                silence=[]
+                print(silence)
+            
             #print(silence)
             if len(silence)==auto_stop_condition:
                 breaker = False
@@ -151,3 +252,4 @@ while audio_stream.is_active():
         wf.close()
     else:
         pass
+
