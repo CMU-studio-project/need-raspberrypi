780d23473f319ce4e0aa7899339752469bc5f76d
jel-lambda <sheee724@gmail.com>
2023-02-17 02:59:59 -0500

    feat: make audio controller using minimum

--- app/audio_controller.py
+++ app/audio_controller.py
@@ -1,14 +1,9 @@
-import os
 import pvporcupine
-import pyaudio
-import struct
-import numpy as np
-import wave
-import math
 import pvcobra
 import sounddevice as sd
-from bulb_controller import LightBulb
-import asyncio
+import struct
+import math
+import numpy as np
 
 class AudioController:
     def __init__(self):
@@ -24,6 +19,19 @@ class AudioController:
     the sample rate.
     Yields/Generates: Frames of the requested duration.
     """
+    def rms( self, data ):
+        count = len(data)/2
+        format = "%dh"%(count)
+        shorts = struct.unpack( format, data )
+        sum_squares = 0.0
+        for sample in shorts:
+            n = sample * (1.0/32768)
+            sum_squares += n*n
+        return math.sqrt( sum_squares / count )
+    
+    def print_sound(indata, outdata, frames, time, status):
+        volume_norm = np.linalg.norm(indata)*10
+        print ("|" * int(volume_norm))
 
     def resample(self, data, input_rate=44100):
         """
@@ -40,125 +48,3 @@ class AudioController:
         resampled_len = int(len(data) * desired_rate / input_rate)
         res = [data[int(i * input_rate / desired_rate)] for i in range(resampled_len)]
         return  res + [res[-1]]
-
-    def record(self, hotword : bool = True, filename: str = "audio/recorded.wav"):
-        
-        pa = pyaudio.PyAudio()
-        recorded = False
-        new_frame_number = int(self.porcupine.frame_length * 441 / 160)
-
-        audio_stream=pa.open(
-            # rate=porcupine.sample_rate,
-            rate = 44100,
-            channels=1,
-            format=pyaudio.paInt16,
-            input=True,
-            frames_per_buffer= new_frame_number)
-
-        volume_queue=[]
-        voice_prob_queue=[]
-        # 마이크를 작동시키는 부분인데, 마이크 연결을 try 로 진행하고, 마이크 연결이 안되어있을 때는 except 구문에서 (마이크 연결이 안되어있다는) 에러 음성을 송출.
-        try:
-            audio_stream=pa.open(
-                # rate=porcupine.sample_rate,
-                rate = 44100,
-                channels=1,
-                format=pyaudio.paInt16,
-                input=True,
-                frames_per_buffer= new_frame_number)
-        except:
-            bulb = LightBulb()
-            asyncio.run(bulb.blink_when_error())
-            os.system("mpg321 './tts-audio/error7_mic.mp3'")
-
-        pcm = audio_stream.read(new_frame_number, exception_on_overflow = False)
-        numpydata = np.frombuffer(pcm, dtype=np.int16)
-        pcm = struct.unpack_from("h" * new_frame_number, pcm)
-        pcm = self.resample(pcm, 44100)
-
-        a = self.cobra.process(pcm)
-        b = np.linalg.norm(numpydata)
-
-        print("voice prob:",a)
-        print("volume:",b)
-        volume_queue.insert(0, b)
-        voice_prob_queue.insert(0, a)
-        if len(volume_queue) > 20:
-            volume_queue.pop()
-            voice_prob_queue.pop()
-
-        keyword_index = self.porcupine.process(pcm)
-        
-        if keyword_index == 0 or not hotword:
-            print("volume_queue:",volume_queue)
-            print("voice_prob_queue:",voice_prob_queue)
-            initial_voice_prob  = sorted(voice_prob_queue)[-5]
-            initial_volume      = volume_queue[voice_prob_queue.index(sorted(voice_prob_queue)[-5]) + 1]
-            # detected 'hey dobby'
-            if hotword:
-                os.system("mpg321 './tts-audio/dobby_is_here.mp3'")
-            print("initial volume:",initial_volume)
-            print("initial voice prob:",initial_voice_prob)
-
-            # set the chunk size of 1024 samples
-            chunk = 44100
-            # sample format
-            FORMAT = pyaudio.paInt16
-            # mono, change to 2 if you want stereo
-            channels = 1
-            # 44100 samples per second
-            sample_rate = 44100
-
-            frames = []
-            silence = []
-
-            record_seconds = 15
-            auto_stop_condition = 3
-            record_speed = 10
-
-            record_seconds *= record_speed 
-            auto_stop_condition *= record_speed 
-
-            breaker = True
-            i = 0
-            print("Recording...")
-            while breaker and i in range(int(sample_rate / chunk * record_seconds)):
-                data = audio_stream.read(int(10000 / record_speed) , exception_on_overflow = False)
-
-                current_voice_prob = self.cobra.process(pcm)
-                current_volume = np.linalg.norm(np.frombuffer(audio_stream.read(new_frame_number, exception_on_overflow = False), dtype=np.int16))
-                
-                if current_voice_prob < initial_voice_prob and current_volume < initial_volume:
-                    print("||||" * (len(silence) + 1))
-                    print("voice prob:",current_voice_prob)
-                    print("volume:",current_volume)
-                    silence.append(1)
-                else:
-                    silence=[]
-                    print("")
-                    print("voice prob:",current_voice_prob)
-                    print("volume:",current_volume)
-                #print(silence)
-                if len(silence)==auto_stop_condition:
-                    breaker = False
-                # if you want to hear your voice while recording
-                # stream.write(data)
-                frames.append(data)
-                i+=1
-            # frames = pcm
-            print("Finished recording.")
-            
-            wf = wave.open(filename, "wb")
-            wf.setnchannels(channels)
-            wf.setsampwidth(pa.get_sample_size(FORMAT))
-            wf.setframerate(sample_rate)
-            wf.writeframes(b"".join(frames))
-            wf.close()
-            recorded = True
-            return recorded
-        else:
-            pass
-
-if __name__ == "__main__":
-    recorder = AudioController()
-    recorder.record(True, "audio/recorded.wav")
--- app/main.py
+++ app/main.py
@@ -2,9 +2,20 @@ from audio_controller import AudioController
 from dotenv import load_dotenv
 import os
 from app import NeedApp
+import os
+import pyaudio
+import struct
+import numpy as np
+import wave
+import sounddevice as sd
+from bulb_controller import LightBulb
+import asyncio
+from audio_controller import AudioController
 
 # # Internet, GCP, bulb connection status check
-# os.system("python error_cases.py")
+# os.system("cd ../")
+# os.system("python Error_message/Error_cases")
+# os.system("cd app")
 
 load_dotenv()
 PROJECT_ID = os.getenv("PROJECT_ID")
@@ -12,8 +23,97 @@ TOPIC_ID = os.getenv("TOPIC_ID")
 DEVICE_ID = os.getenv("DEVICE_ID")
 GOOGLE_APPLICATION_CREDENTIAL = os.getenv("GOOGLE_APPLICATION_CREDENTIAL")
 
+def record(hotword : bool = True, filename: str = "audio/recorded.wav"):
+    audio_set = AudioController()
+    pa = pyaudio.PyAudio()
+    new_frame_number = int(audio_set.porcupine.frame_length * 441 / 160)
+
+    volume_queue=[]
+    voice_prob_queue=[]
+    # 마이크를 작동시키는 부분인데, 마이크 연결을 try 로 진행하고, 마이크 연결이 안되어있을 때는 except 구문에서 (마이크 연결이 안되어있다는) 에러 음성을 송출.
+    try:
+        audio_stream=pa.open(
+            # rate=porcupine.sample_rate,
+            rate = 44100,
+            channels=1,
+            format=pyaudio.paInt16,
+            input=True,
+            frames_per_buffer= new_frame_number)
+    except:
+        bulb = LightBulb()
+        asyncio.run(bulb.blink_when_error())
+        os.system("mpg321 './tts-audio/error7_mic.mp3'")
+
+    while True:
+        pcm = audio_stream.read(new_frame_number, exception_on_overflow = False)
+        numpydata = np.frombuffer(pcm, dtype=np.int16)
+        pcm = struct.unpack_from("h" * new_frame_number, pcm)
+        pcm = audio_set.resample(pcm, 44100)
+
+        keyword_index = audio_set.porcupine.process(pcm)
+        
+        if keyword_index == 0 or not hotword:
+            # initial_volume = audio_set.rms(audio_stream.read(44100 , exception_on_overflow = False))
+            # initial_voice_prob = audio_set.cobra.process(pcm)
+            # detected 'hey dobby'
+            if hotword:
+                os.system("mpg321 './tts-audio/dobby_is_here.mp3'")
+            # print("initial volume:",initial_volume)
+            # print("initial voice prob:",initial_voice_prob)
+
+            # set the chunk size of 1024 samples
+            chunk = 44100
+            # sample format
+            FORMAT = pyaudio.paInt16
+            # mono, change to 2 if you want stereo
+            channels = 1
+            # 44100 samples per second
+            sample_rate = 44100
+
+            frames = []
+            silence = []
+
+            record_seconds = 15
+            auto_stop_condition = 3
+
+            breaker = True
+            i = 0
+            print("Recording...")
+            while breaker and i in range(int(sample_rate / chunk * record_seconds)):
+                data = audio_stream.read(chunk , exception_on_overflow = False)
+
+                if audio_set.rms(data)<0.1:
+                    # print(silence)
+                    # print("volume:",rms(data))
+                    print("voice prob:",audio_set.cobra.process(pcm))
+                    silence.append(1)
+                else:
+                    silence=[]
+                    print(silence)
+                # print(silence)
+                if len(silence)==auto_stop_condition:
+                    breaker = False
+            
+                # if you want to hear your voice while recording
+                # stream.write(data)
+                frames.append(data)
+                i+=1
+            # frames = pcm
+            print("Finished recording.")
+            
+            wf = wave.open(filename, "wb")
+            wf.setnchannels(channels)
+            wf.setsampwidth(pa.get_sample_size(FORMAT))
+            wf.setframerate(sample_rate)
+            wf.writeframes(b"".join(frames))
+            wf.close()
+        
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIAL
+            app = NeedApp(PROJECT_ID, DEVICE_ID, TOPIC_ID)
+            app.run(filename, None)
+        else:
+            pass
+
 if __name__ == "__main__":  
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIAL
-    app = NeedApp(PROJECT_ID, DEVICE_ID, TOPIC_ID)
-    app.run()
+    record()
        
